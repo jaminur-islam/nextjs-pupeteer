625f40e8decc43c6d48a43662f604b37c558c76f
jaminur-islam <jaminurislam250@gmail.com>
2022-11-21 11:39:51 +0600

    added

--- src/pages/api/pdf.ts
+++ src/pages/api/pdf.ts
@@ -1,70 +1,15 @@
-import edgeChromium from 'chrome-aws-lambda'
-
-// Importing Puppeteer core as default otherwise
-// it won't function correctly with "launch()"
 import puppeteer from 'puppeteer-core'
-
-// You may want to change this if you're developing
-// on a platform different from macOS.
-// See https://github.com/vercel/og-image for a more resilient
-// system-agnostic options for Puppeteeer.
+import chromium from 'chrome-aws-lambda'
 const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
-export default async function (req, res) {
-  // Edge executable will return an empty string locally.
-  const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
-  
-  const browser = await puppeteer.launch({
-    executablePath,
-    args: edgeChromium.args,
-    headless: false,
-  })
-  
-  const page = await browser.newPage()
-  await page.goto('https://github.com')
-  // await browser.close()
-  
-  res.send('hello')
-}
-
-
-
-
-
-
-
-
-/* import edgeChromium from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer-core';
-import path from "path"
 export default async function handler(req, res) {
-  const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-  const executablePath = await edgeChromium.executablePath
-  const browser = await puppeteer.launch({
-    executablePath,
-    args: edgeChromium.args,
-    headless: false,
+  const browser = await chromium.puppeteer.launch({
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
   })
-    // const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
-    await page.setContent(`<div> sagor sago ssagor sagor </div>`);
-    await page.setViewport({
-      width: 595,
-      height: 842,
-      deviceScaleFactor: 1,
-    });
-    await page.addStyleTag({ content: "@page { size: A4 landscape; }" });
-    await page.pdf({
-      path: path.join(__dirname, "../../../../public/pdf", "tests.pdf"),
-      printBackground: true,
-      margin: {
-        top: "340px",
-        right: "30px",
-        bottom: "0px",
-        left: "30px",
-      },
-    });
-    await browser.close();    
+  
   res.status(200).json({ name: 'John Doe' })
- 
-} */
+}
---
+++ src/utils/HtmlToImage/screenshot.ts
@@ -0,0 +1,39 @@
+import { Page } from 'puppeteer';
+
+export interface ScreenshotOptions {
+  output?: string;
+  quality?: number;
+  encoding?: string;
+  url: string;
+  transparent: boolean;
+  waitUntil?: any;
+}
+
+export const makeScreenshot = async function (
+  page: Page,
+  {
+    output,
+    quality,
+    encoding,
+    url,
+    transparent = false,
+    waitUntil = 'networkidle0',
+  }: ScreenshotOptions
+): Promise<string> {
+  let screeshotArgs = <any>{};
+
+  const type = 'jpeg';
+  screeshotArgs.quality = quality ? quality : 100;
+
+  await page.goto(url, { waitUntil });
+
+  const buffer = await page.screenshot({
+    path: output,
+    type,
+    omitBackground: transparent,
+    encoding,
+    ...screeshotArgs,
+  });
+
+  return buffer;
+};
